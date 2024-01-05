ccf3931ce1da1ee2f905e8db862c67989d071c44
間嶋 将平 <majima.shohhei@lmi.ne.jp>
2024-01-05 13:16:58 +0900

    edit

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd
 
 st.title("hello world")
 x = 10
 x
+
+df = pd.DataFrame({'col1': [1,2,3]})
+df
+
+uploaded_file = st.file_uploader("ログをアップロードしてください。")
